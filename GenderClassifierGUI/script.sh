6860b415ad8cce7a767fa97cdb670370acce8b26
Jackson <jame1409@colorado.edu>
2016-12-06 23:59:04 -0700

    Updated UI

---
+++ GenderClassifierGUI/script.sh
@@ -0,0 +1,5 @@
+if C:\\cygwin64\\bin\\g++ ../gender.cpp -o gender; then 
+echo "Success!";
+else 
+echo "Failure"; 
+fi
--- GenderClassifierGUI/welcome.cpp
+++ GenderClassifierGUI/welcome.cpp
@@ -1,8 +1,12 @@
 #include "welcome.h"
 #include "ui_welcome.h"
 #include <QFileDialog>
-#include <qmessagebox>
+#include <QMessageBox>
 #include <QLabel>
+#include <QProcess>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsPixmapItem>
 
 Welcome::Welcome(QWidget *parent) :
     QMainWindow(parent),
@@ -10,6 +14,7 @@ Welcome::Welcome(QWidget *parent) :
 {
     ui->setupUi(this);
     connect(ui->selectFile, SIGNAL(clicked()), ui->filenameLabel, SLOT(on_selectFile_clicked()));
+    connect(ui->processFile, SIGNAL(clicked()), ui->filenameLabel, SLOT(on_processFile_clicked()));
 }
 
 Welcome::~Welcome()
@@ -28,15 +33,45 @@ void Welcome::on_selectFile_clicked()
                 "All files (*.*);; JPEG file (*.jpg);; Bitmap file (*.bmp);; Portable Graymap File (*.pgm)"
                 );
 
-    //QMessageBox::information(this, tr("File Name"), filename);
+    //QMessageBox::information(this, tr("Current Directory"), QDir::currentPath());
     if (!filename.isEmpty())
         filenameLabel->setText(dir.relativeFilePath(filename));
         QPushButton * processFile = this->findChild<QPushButton*>("processFile");
         processFile->setEnabled(true);
 }
-/*
+
 void Welcome::on_processFile_clicked()
 {
-    QStringList commandAndParameters<<"gender.cpp"<<
+    QLabel * filenameLabel = this->findChild<QLabel*>("filenameLabel");
+    QStringList arguments;
+    QString program = "../gender";
+    QString path = "../Images/";
+    QString csv = "../att_faces/att.csv";
+    QString image = path.append(filenameLabel->text());
+
+    QMessageBox::information(this, tr("test"), image);
+
+    QGraphicsScene scene;
+    QGraphicsView view(&scene);
+    QPixmap qp = QPixmap(image);
+    if(qp.isNull())
+    {
+        printf("Yes its null\n");
+    }
+    else
+    {
+        printf("HAHA");
+        QGraphicsPixmapItem *item = new QGraphicsPixmapItem(QPixmap(image));
+        scene.addItem(item);
+    }
+    view.show();
+
+    arguments << csv << image;
+    QProcess * process = new QProcess(this);
+    process->start(program, arguments);
+    process->waitForFinished();
+    QString error(process->readAllStandardError());
+    QString output(process->readAllStandardOutput());
+    QMessageBox::information(this, tr("output"), output);
+    QMessageBox::information(this, tr("error"), error);
 }
-*/
